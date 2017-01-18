ab2a1bef3b9951e3cf71f962b14762e886fa5c34
Dante Di Domenico <d.didomenico@channelweb.it>
2017-01-18 10:23:55 +0100

    fix: chmod + chown for be folders in be3-perms.sh

--- be3-perms.sh
+++ be3-perms.sh
@@ -7,6 +7,12 @@
 ################################################################################
 
 HTTPDUSER=`ps aux | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1`
+BEGROUP=bedita
+
+groupname=$1
+if [[ -n "$groupname" ]]; then
+    BEGROUP=$groupname
+fi
 
 if [ -z "$HTTPDUSER" ]; then
     echo "Web server user not found, verify that a webserver service (like Apache2) is up & running"
@@ -23,6 +29,10 @@ echo ""
 for f in bedita-app/tmp bedita-app/webroot/files
 do
         echo "permissions on $f"
+        echo "chown -R :${BEGROUP} ${f}"
+        chown -R :${BEGROUP} ${f}
+        echo "chmod -R g+w ${f}"
+        chmod -R g+w ${f}
         echo "setfacl -R -m u:${HTTPDUSER}:rwx ${f}"
         setfacl -R -m u:${HTTPDUSER}:rwx ${f}
         echo "setfacl -R -d -m u:${HTTPDUSER}:rwx ${f}"
@@ -36,6 +46,10 @@ echo ""
 for f in `ls -d frontends/*/`
 do
         echo "permissions on $f"
+        echo "chown -R :${BEGROUP} ${f}tmp"
+        chown -R :${BEGROUP} ${f}tmp
+        echo "chmod -R g+w ${f}tmp"
+        chmod -R g+w ${f}tmp
         echo "setfacl -R -m u:${HTTPDUSER}:rwx ${f}tmp"
         setfacl -R -m u:${HTTPDUSER}:rwx ${f}tmp
         echo "setfacl -R -d -m u:${HTTPDUSER}:rwx ${f}tmp"
