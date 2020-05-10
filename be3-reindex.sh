7148b01c2e9aa429e40cbf08fe95d5175d19f5a7
leo <mainardi.leo@gmail.com>
2020-05-10 11:42:15 +0200

    - refactor: using functions to clean code - fix: wait for first type to end when called with `--delete` flag

--- be3-reindex.sh
+++ be3-reindex.sh
@@ -1,22 +1,55 @@
 #!/bin/bash
 
 function print_usage () {
-  printf "Usage: ./reindex.sh -t|--types <types> [-c|--concurrency <number>] [-s|--session <name>] [-d|--delete]\n"
+  printf "Usage: ./be3-reindex.sh -t|--types <types> [-c|--concurrency <number>] [-s|--session <name>] [-d|--delete]\n"
   printf "\n"
   printf "Available parameters:\n"
-  printf "\t-h|--help\t\tprint this help\n"
-  printf "\t-t|--types\t\tcomma-separated list of types\n"
-  printf "\t-c|--concurrency\treindex this many types concurrently\n"
-  printf "\t-s|--session\t\ttmux session name, will be created if doesn't exists\n"
-  printf "\t-d|--delete\t\tdelete current index before starting\n"
+  printf "\t-h|--help\t\t print this help\n"
+  printf "\t-t|--types\t\t comma-separated list of types\n"
+  printf "\t-c|--concurrency\t reindex this many types concurrently\n"
+  printf "\t-s|--session\t\t tmux session name, will be created if doesn't exists\n"
+  printf "\t-d|--delete\t\t delete current index before starting\n"
+  printf "\t           \t\t WARN: be sure that the first type has few objects, otherwise it will delay all others!\n"
+}
+
+function session_exist () {
+  tmux has-session -t "$SESSION" 2> /dev/null
+}
+
+function select_window () {
+  tmux select-window -t "$SESSION":0
+}
+
+function create_session () {
+  tmux new-session -d -s "$SESSION"
+}
+
+function kill_session () {
+  tmux kill-session -t "$SESSION"
+}
+
+function split_window () {
+  tmux split-window -t "$SESSION":0.0 "$1"
+  tmux select-layout -t "$SESSION":0 tiled
+}
+
+function count_panes () {
+  tmux list-panes -t "$SESSION":0 | wc -l
+}
+
+function current_date () {
+  date +"%F %T"
+}
+
+function current_timestamp () {
+  date +"%s"
 }
 
 # default
 SESSION='reindex'
-CONCURRENCY=4
+MAX_PANES=4
 TYPES=()
 DELETE=0
-INITIAL_PANES=0
 
 if [ "$#" -eq "0" ]; then
   print_usage
@@ -24,7 +57,7 @@ if [ "$#" -eq "0" ]; then
 fi
 
 # read parameters (thanks https://stackoverflow.com/a/14203146/2270403)
-while [[ "$#" -gt "0" ]]; do
+while [ "$#" -gt "0" ]; do
   key="$1"
 
   case $key in
@@ -40,7 +73,7 @@ while [[ "$#" -gt "0" ]]; do
       shift
       ;;
       -c|--concurrency)
-      CONCURRENCY="$2"
+      MAX_PANES="$2"
       shift
       shift
       ;;
@@ -64,56 +97,59 @@ if [ "${#TYPES[@]}" -eq "0" ]; then
   exit 1
 fi
 
-START_TIME="$(date +"%s")"
-COMMAND="echo Reindex of type ${TYPES[0]} && ./cake.sh dbadmin rebuildIndex -type ${TYPES[0]}"
+printf "[%s] Starting reindex of %s types, %s at a time\n" "$(current_date)" "${#TYPES[@]}" "$MAX_PANES"
+START_TIME="$(current_timestamp)"
+DID_CREATE=0
 
-# handle delete parameter
-if [ "$DELETE" -eq "0" ]; then
-  printf "[%s] Starting reindex for type '%s'\n" "$(date +"%F %T")" "${TYPES[0]}"
+if session_exist; then
+  printf "[%s] Using tmux session '%s'\n" "$(current_date)" "$SESSION"
 else
-  COMMAND="$COMMAND -delete"
-  printf "[%s] Deleting old index and starting reindex for type '%s'\n" "$(date +"%F %T")" "${TYPES[0]}"
+  create_session
+  DID_CREATE=1
+  printf "[%s] Created tmux session '%s'\n" "$(current_date)" "$SESSION"
 fi
 
-# start reindex for first type, creating tmux session if needed
-if ! tmux has-session -t "$SESSION" > /dev/null 2>&1; then
-  tmux new-session -d -s "$SESSION" "$COMMAND"
-  tmux select-window -t "$SESSION":0
-else
-  tmux select-window -t "$SESSION":0
-  # account for panels already open
-  INITIAL_PANES="$(tmux list-panes -s -t "$SESSION" | wc -l)"
-  ((CONCURRENCY += INITIAL_PANES))
-  tmux split-window -t "$SESSION":0.0 "$COMMAND"
-  tmux select-layout -t "$SESSION":0 tiled
+INITIAL_PANES="$(count_panes)"
+(( MAX_PANES += INITIAL_PANES ))
+select_window
+
+if [ "$DELETE" -eq "1" ]; then
+  # pop first type from array
+  type="${TYPES[0]}"
+  TYPES=("${TYPES[@]:1}")
+
+  printf "[%s] Deleting old index and starting reindex of type '%s'\n" "$(current_date)" "$type"
+  split_window "echo Reindex of type $type && ./cake.sh dbadmin rebuildIndex -delete -type $type"
+
+  # wait first type to finish
+  while [ "$(count_panes)" -gt "$INITIAL_PANES" ]; do
+    sleep 1
+  done
 fi
 
-# wait an arbitrary amount of time for index deletion to take effect
-sleep 5
-
-# reindex all types
-for type in "${TYPES[@]:1}"; do
-  while [ "$(tmux list-panes -s -t "$SESSION" | wc -l)" -ge "$CONCURRENCY" ]; do
-    sleep 3
+for type in "${TYPES[@]}"; do
+  while [ "$(count_panes)" -ge "$MAX_PANES" ]; do
+    sleep 1
   done
 
-  printf "[%s] Starting reindex for type '%s'\n" "$(date +"%F %T")" "$type"
-  tmux split-window -t "$SESSION":0.0 "echo Reindex of type $type && ./cake.sh dbadmin rebuildIndex -type $type"
-  tmux select-layout -t "$SESSION":0 tiled
+  printf "[%s] Starting reindex of type '%s'\n" "$(current_date)" "$type"
+  split_window "echo Reindex of type $type && ./cake.sh dbadmin rebuildIndex -type $type"
   sleep 1
 done
 
-printf "[%s] All types started, waiting for reindex to finish..." "$(date +"%F %T")"
+printf "[%s] All types started, waiting for reindex to finish..." "$(current_date)"
 
 # wait for all types to finish
-while [ "$(tmux list-panes -s -t "$SESSION" | wc -l)" -gt "$INITIAL_PANES" ]; do
-  sleep 3
-
-  if ! tmux has-session -t "$SESSION" > /dev/null 2>&1; then
-    break
-  fi
+while [ "$(count_panes)" -gt "$INITIAL_PANES" ]; do
+  sleep 1
 done
 
 printf "done!\n"
-ELAPSED_TIME=$(($(date +"%s") - "$START_TIME"))
-printf "[%s] Total elapsed time: %s\n" "$(date +"%F %T")" "$(date -u -d @"$ELAPSED_TIME" +"%T")"
+END_TIME="$(current_timestamp)"
+ELAPSED_TIME="$((END_TIME - START_TIME))"
+printf "[%s] Total elapsed time: %s\n" "$(current_date)" "$(date -u -d @"$ELAPSED_TIME" +"%T")"
+
+# kill session if we created it
+if [ "$DID_CREATE" -eq "1" ]; then
+  kill_session
+fi
